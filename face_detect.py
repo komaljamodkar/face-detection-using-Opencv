58391a3d5ef7dc464fb6db544e09bfadc08bb668
koli0808 <48888350+koli0808@users.noreply.github.com>
2019-04-17 17:47:46 +0530

    Create face_detect.py

---
+++ face_detect.py
@@ -0,0 +1,24 @@
+#image face detection in webcam
+
+import numpy as np
+import cv2
+
+face_cascade=cv2.CascadeClassifier('/home/swati/Desktop/haarcascade_frontalface_default.xml')
+
+videocapture=cv2.VideoCapture(0)
+scale_factor=1.3
+
+while 1:
+    ret, pic = videocapture.read()
+    faces = face_cascade.detectMultiScale(pic, scale_factor,5)
+
+    for(x,y,w,h) in faces:
+        cv2.rectangle(pic,(x,y),(x+w,y+h),(255,0,0),2)
+        font=cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(pic,"me",(x,y),font,2,(255,255,255),2,cv2.LINE_AA)
+
+    print("number of faces {}".format(len(faces)))
+    cv2.imshow('face',pic)
+    if cv2.waitKey(30) & 0xFF == ord('q'):
+        break
+cv2.destroyAllWindows()
